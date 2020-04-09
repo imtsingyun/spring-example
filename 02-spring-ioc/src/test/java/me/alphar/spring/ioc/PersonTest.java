b094b31df4f5907fab5b77809f84a24f8dedc05a
suryaiu <suryaiwang@gmail.com>
2020-04-09 17:45:45 +0800

    test

--- 02-spring-ioc/src/test/java/me/alphar/spring/ioc/PersonTest.java
+++ 02-spring-ioc/src/test/java/me/alphar/spring/ioc/PersonTest.java
@@ -1,31 +1,35 @@
 package me.alphar.spring.ioc;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:/applicationContext.xml")
 public class PersonTest {
 
     // 使用 BeanFactory 获取对象
+    // beanFactory 有延迟初始化的特点，再创建Spring 容器的时候不回去立马创建对象
+    // 而是等到从容器中获取对象的时候才会创建对象
     @Test
     public void testBeanFactory() {
         Resource resources = new ClassPathResource("applicationContext.xml");
         BeanFactory beanFactory = new XmlBeanFactory(resources);
+        System.out.println("-------------------------------------------");
         Person person = beanFactory.getBean("person", Person.class);
         System.out.println(person);
     }
 
     // 使用 ApplicationContext 获取对象
+    // ApplicationContext 在创建Spring容器的时候, 就会立马创初始化，而不会等到获取bean的时候才去创建
     @Test
     public void testApplicationContext() {
-
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        System.out.println("-------------------------------------------");
+        Person p = ctx.getBean("person", Person.class);
+        System.out.println(p);
     }
 }
